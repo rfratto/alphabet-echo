187fc51a2007dd6b3a6900b0f9de0dbea1a92d66
Robert Fratto <robertfratto@gmail.com>
2021-04-28 14:13:20 -0400

    emphasis feature

--- main.go
+++ main.go
@@ -10,20 +10,17 @@ import (
 )
 
 func main() {
-	var (
-		useWhite bool
-	)
+	var opts aecho.Options
 
 	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	fs.BoolVar(&useWhite, "white", false, "print the not-as-good white letters instead of yellow")
+	fs.BoolVar(&opts.UseWhite, "white", false, "print the not-as-good white letters instead of yellow")
+	fs.BoolVar(&opts.Emphasize, "emphasize", false, "add 👏 in between words")
 
 	if err := fs.Parse(os.Args[1:]); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	output := aecho.Transform(strings.Join(fs.Args(), ` `), aecho.Options{
-		UseWhite: useWhite,
-	})
+	output := aecho.Transform(strings.Join(fs.Args(), ` `), opts)
 	fmt.Printf("%s\n", output)
 }
