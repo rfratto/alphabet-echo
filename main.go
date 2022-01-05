5be386318b25445b60a4aae523339c2e2cd3b9e7
Robert Fratto <robertfratto@gmail.com>
2022-01-04 19:27:10 -0500

    support reading from stdin

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -10,6 +11,13 @@ import (
 )
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	var opts aecho.Options
 
 	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
@@ -17,10 +25,22 @@ func main() {
 	fs.BoolVar(&opts.Emphasize, "emphasize", false, "add 👏 in between words")
 
 	if err := fs.Parse(os.Args[1:]); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 
-	output := aecho.Transform(strings.Join(fs.Args(), ` `), opts)
+	// Prefer stdin if it has data to read
+	var input string
+	if fi, err := os.Stdin.Stat(); err == nil && fi.Mode()&os.ModeCharDevice == 0 {
+		bb, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+		input = string(bb)
+	} else {
+		input = strings.Join(fs.Args(), ` `)
+	}
+
+	output := aecho.Transform(input, opts)
 	fmt.Printf("%s\n", output)
+	return nil
 }
